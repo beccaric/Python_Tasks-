2ccc167682c30ad88c2addb3becae2e38f3b4e00
becca <93043262+beccaric@users.noreply.github.com>
2022-10-02 12:21:44 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+#Python Tasks
+#27.09.2022
+
+
+salary = 15 
+print (salary)
+salary = salary + 50
+print (salary)
+salary = salary + salary
+print (salary)
+salary = salary * salary
+print (salary)
+
+#Two variables: Price and Stock 
+price = 10
+stock = 5
+
+print (price)
+print (stock)
+print (price + stock)
+#only use speech marks when you want to print out some text, no speech marks if you want the answer to something 
